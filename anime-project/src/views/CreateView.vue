5225c2b243aa4d629e575dce5e99e419124cc16d
Ettore Occhiena Elias <ettorepri@gmail.com>
2023-03-21 20:08:26 +0100

    Update CreateView.vue

--- anime-project/src/views/CreateView.vue
+++ anime-project/src/views/CreateView.vue
@@ -4,48 +4,58 @@
 
 <template>
 
-    <div class="about">
-      <h1>Create a new Anime</h1>
-    
-      <form action="submit">
-        <input id="tentei"
-        class="imp-create"
-        type="text"
-        placeholder="Name of Anime">
-        <input 
-        class="imp-create"
-        type="number"
-        placeholder="When it was released">
-        <input 
-        class="imp-create"
-        type="text"
-        placeholder="Episodios released">
-        <input 
-        class="imp-create"
-        type="text"
-        placeholder="Genre">
-        <input 
-        class="imp-create"
-        type="text"
-        placeholder="written by">
-        <input 
-        class="imp-create"
-        type="text"
-        placeholder="Description"
-        >
 
-
-
-        <p></p>
-
-
-
-      </form>
-
-    </div>
+    <main>
+      <div class="about">
+        <h1>Create a new Anime</h1>
+      
+        <form action="submit" id="form-create">
+          <input id="tentei"
+          class="imp-create"
+          type="text"
+          placeholder="Name of Anime">
+          <input
+          class="imp-create"
+          type="number"
+          placeholder="When it was released">
+          <input
+          class="imp-create"
+          type="text"
+          placeholder="Episodios released">
+          <input
+          class="imp-create"
+          type="text"
+          placeholder="Genre">
+          <input
+          class="imp-create"
+          type="text"
+          placeholder="written by">
+          <input
+          class="imp-create"
+          type="text"
+          placeholder="Description"
+          >
+        </form>
+      </div>
+    </main>
   </template>
   
-  <style scoped>
+  <style >
+.about{
+  width: 100%;
+  text-align: center;
+  background-color:var(--color-background);
+}
+h1{
+  margin: auto;
+}
+.imp-create{
+    width: 80%;
+    padding: 10px;
+    border-radius: 10px;
+    margin: auto;
+    
+  }
  
  
   </style>
