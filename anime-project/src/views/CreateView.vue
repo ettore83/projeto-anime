62dd3815471ba1918a6f8e0f9acc3245144b3b30
Ettore Occhiena Elias <ettorepri@gmail.com>
2023-03-22 18:59:49 +0100

    Update CreateView.vue

--- anime-project/src/views/CreateView.vue
+++ anime-project/src/views/CreateView.vue
@@ -8,31 +8,41 @@
         <h1>Create a new Anime</h1>
       
         <form action="submit" id="form-create">
+          <p>Anime name</p>
           <input id="tentei"
           class="imp-create"
           type="text"
           placeholder="Name of Anime">
+          <p>Release date</p>
           <input
           class="imp-create"
           type="number"
           placeholder="When it was released">
+          <p>Quantity episodios</p>
           <input
           class="imp-create"
           type="text"
           placeholder="Episodios released">
+          <p>Genre type</p>
           <input
           class="imp-create"
           type="text"
           placeholder="Genre">
+          <p>Written by</p>
           <input
           class="imp-create"
           type="text"
           placeholder="written by">
+          <p>Description</p>
           <input
           class="imp-create"
           type="text"
           placeholder="Description"
           >
+          <br>
+          <button id="btn-post">
+          POST
+          </button>
         </form>
       </div>
     </main>
@@ -42,18 +52,35 @@
 .about{
   width: 100%;
   text-align: center;
-  background-color:var(--color-background);
+  background-color:red;
+  height: 900px;
 }
 h1{
   margin: auto;
 }
+p{
+  color: aliceblue;
+  width: 80%;
+  margin: auto;
+  text-align: left;
+  margin-top: 20px;
+  
+}
 .imp-create{
     width: 80%;
-    padding: 10px;
+    padding: 15px;
     border-radius: 10px;
     margin: auto;
+}  
         
-  }
+ button{
+  width: 80%
+  padding :15px;
+  border-radius:10px;
+  margin:auto;
+}       
+
+  
  
  
   </style>
