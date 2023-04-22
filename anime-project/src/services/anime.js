68249f0c87163fd0281b6250b5060f6dfd0b2a48
Ettore Occhiena Elias <ettorepri@gmail.com>
2023-04-22 11:48:47 +0200

    api

---
+++ anime-project/src/services/anime.js
@@ -0,0 +1,14 @@
+import { APISettings } from "./config";import axios from "axios";
+
+export default {
+    index() { console.log(APISettings);
+    return axios.get(APISettings.baseURL + "houses", {
+        headers: APISettings.headers,
+    });
+},
+    create(params) {console.log(APISettings);
+    return axios.post(APISettings.baseURL + "houses", params, {
+        headers: APISettings.headers,
+    });
+},
+}
---
+++ anime-project/src/services/config.js
@@ -0,0 +1,9 @@
+export const APISettings = {
+    token: "",
+    headers: {
+        accept: "applicattion/Json", "X-Api-Key": "5ropeyFvE4d_qg3iWfPaKt8bAmn7cjRQ", 
+        //next line comand to help upload image  
+        "Content-Type": "multipart/form-data",
+     },
+      baseURL: "https://api.intern.d-tt.nl/api/",
+    }; 
